67ebeab16e01e1cd51908771be3372426a8a78ad
YasiraBanuka <thybanuka18@gmail.com>
2024-06-20 16:07:57 +0530

    HomePage UI done

--- jobhub_frontend/lib/views/ui/homepage.dart
+++ jobhub_frontend/lib/views/ui/homepage.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
 import 'package:jobhub/views/common/app_bar.dart';
 import 'package:jobhub/views/common/drawer/drawer_widget.dart';
+import 'package:jobhub/views/common/exports.dart';
+import 'package:jobhub/views/common/heading_widget.dart';
+import 'package:jobhub/views/common/height_spacer.dart';
+import 'package:jobhub/views/common/search.dart';
+import 'package:jobhub/views/common/vertical_tile.dart';
+import 'package:jobhub/views/ui/jobs/job_page.dart';
+import 'package:jobhub/views/ui/jobs/widgets/horizontal_tile.dart';
+import 'package:jobhub/views/ui/search/searchpage.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -29,6 +38,57 @@ class _HomePageState extends State<HomePage> {
           child: Padding(
             padding: EdgeInsets.all(12.0.h),
             child: const DrawerWidget(),
+          ),
+        ),
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.w),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const HeightSpacer(size: 10),
+                Text(
+                  "Search \nFind & Apply",
+                  style: appstyle(40, Color(kDark.value), FontWeight.bold),
+                ),
+                const HeightSpacer(size: 40),
+
+                /// Search bar
+                SearchWidget(
+                  onTap: () {
+                    Get.to(() => const SearchPage());
+                  },
+                ),
+
+                const HeightSpacer(size: 30),
+
+                /// Heading --- Popular Jobs
+                HeadingWidget(text: "Popular Jobs", onTap: () {}),
+                const HeightSpacer(size: 15),
+                // Popular Jobs
+                SizedBox(
+                  height: hieght * 0.28,
+                  child: ListView.builder(
+                      itemCount: 4,
+                      scrollDirection: Axis.horizontal,
+                      itemBuilder: (context, index) {
+                        return JobHorizontalTile(
+                          onTap: () {
+                            Get.to(() => const JobPage());
+                          },
+                        );
+                      }),
+                ),
+                const HeightSpacer(size: 20),
+
+                /// Heading --- Recently Posted
+                HeadingWidget(text: "Recently Posted", onTap: () {}),
+                const HeightSpacer(size: 15),
+                VerticalTile()
+              ],
+            ),
           ),
         ),
       ),
--- jobhub_frontend/lib/views/ui/jobs/widgets/horizontal_tile.dart
+++ jobhub_frontend/lib/views/ui/jobs/widgets/horizontal_tile.dart
@@ -1,10 +1,88 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_vector_icons/flutter_vector_icons.dart';
+import 'package:jobhub/views/common/exports.dart';
+import 'package:jobhub/views/common/height_spacer.dart';
 
 class JobHorizontalTile extends StatelessWidget {
-  const JobHorizontalTile({super.key});
+  const JobHorizontalTile({super.key, this.onTap});
+
+  final void Function()? onTap;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return GestureDetector(
+      onTap: onTap,
+      child: Padding(
+        padding: EdgeInsets.only(right: 12.w),
+        child: Container(
+          padding: EdgeInsetsDirectional.symmetric(
+            horizontal: 20.w,
+            vertical: 20.h,
+          ),
+          width: width * 0.7,
+          height: hieght * 0.27,
+          color: Color(kLightGrey.value),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  const CircleAvatar(
+                    backgroundImage: AssetImage("assets/images/Facebook.png"),
+                  ),
+                  const SizedBox(width: 15),
+                  ReusableText(
+                    text: "Facebook",
+                    style: appstyle(26, Color(kDark.value), FontWeight.w600),
+                  ),
+                ],
+              ),
+              const HeightSpacer(size: 15),
+
+              /// Job title
+              ReusableText(
+                text: "Flutter Developer",
+                style: appstyle(20, Color(kDark.value), FontWeight.w600),
+              ),
+
+              /// Location
+              ReusableText(
+                text: "Washington DC",
+                style: appstyle(16, Color(kDarkGrey.value), FontWeight.w600),
+              ),
+
+              const HeightSpacer(size: 20),
+
+              /// Salary
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      ReusableText(
+                        text: "15K",
+                        style:
+                            appstyle(23, Color(kDark.value), FontWeight.w600),
+                      ),
+                      ReusableText(
+                        text: "/monthly",
+                        style:
+                            appstyle(23, Color(kDarkGrey.value), FontWeight.w600),
+                      ),
+                    ],
+                  ),
+                  CircleAvatar(
+                    radius: 18,
+                    backgroundColor: Color(kLight.value),
+                    child: const Icon(Ionicons.chevron_forward),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
